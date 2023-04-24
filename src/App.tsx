b3f16801379b80c7c4ef949810226acd28866230
Harshdeep Singh <harsh.code97@gmail.com>
2023-04-25 01:43:23 +0530

    feat: Initialize global store using 'Redux-toolkit'

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import { Provider } from "react-redux";
 import HomePage from "./pages/Home";
 import ErrorPage from "./pages/Error";
 import Root from "./components/layout/Root";
+import { store } from "./store/store";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -17,7 +19,11 @@ const router = createBrowserRouter(
 );
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
 }
 
 export default App;
