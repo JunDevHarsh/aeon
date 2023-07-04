9a710d1b33b09eca35e47d920bef0ba5051cbc75
Harshdeep Singh <harsh.code97@gmail.com>
2023-07-04 12:34:06 +0530

    style: Add layout and add padding to description

--- src/components/card/AddOnDriver.tsx
+++ src/components/card/AddOnDriver.tsx
@@ -194,85 +194,87 @@ function AddOnDriverPopup({
         onClick={closePopup}
         className="fixed top-0 left-0 right-0 bottom-0 h-full w-full bg-gradient-to-tr from-[rgba(0,0,0,0.7)] to-[rgba(0,0,0,0.7)] z-[20]F"
       />
-      <div className="relative min-w-[300px] max-w-lg h-auto bg-white rounded">
-        <div className="relative px-4 py-2 flex flex-row items-center justify-end w-full h-auto border-b-2 border-solid border-b-[#c3c3c3]">
-          <button
-            className="inline-block w-auto h-auto rounded"
-            onClick={closePopup}
-          >
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+      <div className="relative p-4">
+        <div className="relative min-w-[300px] max-w-lg h-auto bg-white rounded">
+          <div className="relative px-4 py-2 flex flex-row items-center justify-end w-full h-auto border-b-2 border-solid border-b-[#c3c3c3]">
+            <button
+              className="inline-block w-auto h-auto rounded"
+              onClick={closePopup}
             >
-              <path
-                d="M18 6L6 18"
-                stroke="#6C6F75"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M6 6L18 18"
-                stroke="#6C6F75"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </button>
-        </div>
-        <div className="relative px-4 pt-4 pb-6 w-full">
-          <div className="relative inline-block w-full">
-            <h3 className="text-lg text-left text-primary-black font-medium">
-              Select the preferred option for additional driver
-            </h3>
-            <div className="relative mt-2 w-full">
-              <RadioField
-                title="Select Option"
-                options={[
-                  {
-                    title: "Named Drivers",
-                    value: "named",
-                    description:
-                      "This add-on coverage extends your insurance policy to cover the driver named in your insurance policy.",
-                  },
-                  {
-                    title: "Unlimited Drivers - RM 20",
-                    value: "unlimited",
-                    description:
-                      "This add-on coverage extends your insurance policy to cover anyone who drives your car. You do not have to name anyone in your insurance policy.",
-                  },
-                ]}
-                selectedValue={value}
-                updateValue={updateStateValue}
-              />
-            </div>
-            {error && (
-              <span
-                className="relative text-sm text-left font-medium text-red-600"
-                role="alert"
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                {error}
-              </span>
-            )}
+                <path
+                  d="M18 6L6 18"
+                  stroke="#6C6F75"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M6 6L18 18"
+                  stroke="#6C6F75"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </button>
           </div>
-        </div>
-        <div className="px-4 py-2 flex items-center justify-end w-full">
-          <button
-            onClick={handleOnSave}
-            className="inline-block px-4 py-2 text-sm text-center text-white bg-primary-blue font-medium rounded"
-          >
-            Save
-          </button>
-          <button
-            onClick={handleOnCancel}
-            className="inline-block ml-2 px-4 py-2 text-sm text-center text-white bg-primary-blue font-medium rounded"
-          >
-            Cancel
-          </button>
+          <div className="relative px-4 pt-4 pb-6 w-full">
+            <div className="relative inline-block w-full">
+              <h3 className="text-lg text-left text-primary-black font-medium">
+                Select the preferred option for additional driver
+              </h3>
+              <div className="relative mt-2 w-full">
+                <RadioField
+                  title="Select Option"
+                  options={[
+                    {
+                      title: "Named Drivers",
+                      value: "named",
+                      description:
+                        "This add-on coverage extends your insurance policy to cover the driver named in your insurance policy.",
+                    },
+                    {
+                      title: "Unlimited Drivers - RM 20",
+                      value: "unlimited",
+                      description:
+                        "This add-on coverage extends your insurance policy to cover anyone who drives your car. You do not have to name anyone in your insurance policy.",
+                    },
+                  ]}
+                  selectedValue={value}
+                  updateValue={updateStateValue}
+                />
+              </div>
+              {error && (
+                <span
+                  className="relative text-sm text-left font-medium text-red-600"
+                  role="alert"
+                >
+                  {error}
+                </span>
+              )}
+            </div>
+          </div>
+          <div className="px-4 py-2 flex items-center justify-end w-full">
+            <button
+              onClick={handleOnSave}
+              className="inline-block px-4 py-2 text-sm text-center text-white bg-primary-blue font-medium rounded"
+            >
+              Save
+            </button>
+            <button
+              onClick={handleOnCancel}
+              className="inline-block ml-2 px-4 py-2 text-sm text-center text-white bg-primary-blue font-medium rounded"
+            >
+              Cancel
+            </button>
+          </div>
         </div>
       </div>
     </Popup>
@@ -339,7 +341,7 @@ function RadioField({
                 </span>
               </label>
             </div>
-            <div className="relative ml-6 w-full">
+            <div className="relative px-7 w-full">
               <p className="text-sm text-left text-primary-black font-medium">
                 {description}
               </p>
