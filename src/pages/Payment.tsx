930b0beb7907b404e7d4d0c2e0626b1e6ec35e52
Harshdeep Singh <harsh.code97@gmail.com>
2023-07-12 00:22:51 +0530

    fix: Update payment

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -1,70 +1,82 @@
-import { useState } from "react";
-import PaymentProvidersImg from "../assets/images/payment_providers.png";
+import { useEffect, useState } from "react";
 import GuyImg from "../assets/images/guy_holding_stick.png";
 import PaymentSuccessfulContainer from "../components/container/PaymentSuccess";
 import PaymentFailedContainer from "../components/container/PaymentFailed";
-import { useSelector } from "react-redux";
-import { RootState } from "../store/store";
+import { useNavigate, useSearchParams } from "react-router-dom";
+// import { checkSession, getPaymentConfig } from "../services/apiServices";
+// import { SHA256 } from "../utils/helpers";
 
 interface PaymentStatusType {
   price: string;
   loading: boolean;
-  status: "success" | "failed" | null;
+  status: "success" | "failed" | "loading" | "pending";
   transactionId: string;
 }
 
 const PaymentPage = () => {
-  const amount = useSelector((state: RootState) => state.insurance.finalPrice);
+  // const amount = useSelector((state: RootState) => state.insurance.finalPrice);
   const [paymentStatus, setPaymentStatus] = useState<PaymentStatusType>({
     price: "",
     loading: false,
-    status: null,
+    status: "loading",
     transactionId: "",
   });
 
-  async function handleOnClick() {
-    setPaymentStatus((prev) => ({ ...prev, loading: true }));
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  function updatePaymentStatus(
+    amount: string,
+    transId: string,
+    status: string
+  ) {
     try {
-      const res: any = await makePayment(amount || "400");
-      const transId = res.transactionId;
-      setPaymentStatus((_) => ({
-        price: res.amount,
-        loading: false,
-        status: "success",
-        transactionId: transId,
-      }));
+      if (status === "1") {
+        setPaymentStatus((prev) => ({
+          ...prev,
+          loading: false,
+          price: amount,
+          status: "success",
+          transactionId: transId,
+        }));
+        // return;
+      } else if (status === "0") {
+        setPaymentStatus((prev) => ({
+          ...prev,
+          status: "failed",
+          price: amount,
+          transactionId: transId,
+        }));
+      } else {
+        setPaymentStatus((prev) => ({
+          ...prev,
+          status: "failed",
+        }));
+      }
+      // const credentialResponse: any = await checkSession(null, null);
+      // const paymentConfig = await getPaymentConfig(
+      // credentialResponse.session.sessionName
+      // );
+      // const signature = SHA256(
+      //   `3jeL1HvYCE${paymentConfig.MerchantCode}${refNo}100MYR`
+      // );
     } catch (err) {
       console.log(err);
-      setPaymentStatus((prev) => ({
-        ...prev,
-        loading: false,
-        status: "failed",
-      }));
     }
   }
 
-  function makePayment(amount: string) {
-    return new Promise((res, _) => {
-      setTimeout(() => {
-        res({ transactionId: "KJHKH432", amount: amount });
-      }, 2000);
-    });
-  }
-
-  if (paymentStatus.loading) {
-    return (
-      <div className="mx-auto py-auto py-10 flex flex-col items-center justify-between max-w-3xl w-full">
-        <img
-          src={GuyImg}
-          alt="guy-holding-stick-in-his-hand"
-          className="mx-auto"
-        />
-        <p className="text-3xl text-center text-primary-black font-bold">
-          Loading
-        </p>
-      </div>
-    );
-  }
+  useEffect(() => {
+    const amount = searchParams.get("amount");
+    const status = searchParams.get("status");
+    const transId = searchParams.get("trans_id");
+    // const hashString = searchParams.get("hashstring");
+    // const refNo = searchParams.get("refno");
+    if (amount && status && transId) {
+      updatePaymentStatus(amount, transId, status);
+    } else {
+      navigate("/");
+    }
+  }, []);
 
   if (paymentStatus.status === "success") {
     return (
@@ -80,15 +92,15 @@ const PaymentPage = () => {
   return (
     <>
       <div className="relative py-10 flex items-center justify-center mx-auto max-w-5xl w-full">
-        <div
-          className="flex items-center justify-center max-w-3xl w-full"
-          onClick={handleOnClick}
-        >
+        <div className="flex flex-col items-center justify-between w-full">
           <img
-            src={PaymentProvidersImg}
-            alt="payment-provider-images"
-            className="w-full min-h-[calc(100vh-224px)] h-auto"
+            src={GuyImg}
+            alt="guy-holding-stick-in-his-hand"
+            className="mx-auto"
           />
+          <p className="text-3xl text-center text-primary-black font-bold">
+            Loading
+          </p>
         </div>
       </div>
     </>
